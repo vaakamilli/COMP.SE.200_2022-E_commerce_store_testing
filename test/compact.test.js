0e85b9927ab087613e8fca99cfb214467c6ac473
Miika Valli <miika.valli@tuni.fi>
2022-11-14 21:20:01 +0200

    Added a test for compact.js

---
+++ test/compact.test.js
@@ -0,0 +1,9 @@
+var chai = require("chai")
+var compact = require ("../src/compact.js")
+const expect = chai.expect
+
+describe("Compact", () => {
+    it("Creates an array with all falsey values removed", () =>{
+        expect(compact([0, 1, false, 2, '', 3])).to.deep.equal([1, 2, 3])
+    });
+   })
